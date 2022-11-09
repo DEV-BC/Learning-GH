1f8edaa09a26f7b51443bfd8ecbc2ee1a29922a5
DEV-BC <carrbrandon1716@gmail.com>
2022-11-08 20:57:52 -0600

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by DEV-BC on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
